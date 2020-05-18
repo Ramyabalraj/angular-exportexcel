5b9a931bde4e6ee820dd97cd3923c0e255d930bd
Ramyabalraj <ramya.balraj@kgisl.com>
2020-05-18 12:35:38 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { ExcelService } from "./excel.service";
 import { map } from "rxjs/operators";
 import moment from "moment";
 import { DatePipe } from "@angular/common";
-import employee  from  './employee.json';
+import employee from "./employee.json";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
@@ -22,10 +22,10 @@ export class AppComponent {
   date: string;
   fdate: Date = new Date("2020-04-01");
   excel: [] = [];
-  excel2:[]=[];
+  excel2: [] = [];
   new_date = new Date();
   i: number = 1;
- json={
+  json = {
     status: 200,
     message: "Records Found",
     response: {
@@ -226,99 +226,111 @@ export class AppComponent {
       }
     }
   };
- // products: any = (employee as any).default;
+  // products: any = (employee as any).default;
   generateExcel() {
-    
     this.result.push(this.json);
     console.log(this.result);
     var month = this.fdate.getMonth();
-    var dateee = this.fdate.getDate();
-     var mon;
-     var datee;
+    var getdate = this.fdate.getDate();
+    var finalmonth;
+    var finaldate;
 
-this.excel2.push(this.result.map(x => x.response[22910].AssociateDetail.associate_code));
-this.excel2.push(this.result.map(x => x.response[22910].AssociateDetail.associate_name));
-this.excel2.push(this.result.map(x => x.response[22910].AssociateDetail.email));
-this.excel2.push(this.result.map(x => x.response[22910].AssociateDetail.title));
-this.excel2.push(this.result.map(x => x.response[22910].AssociateDetail.department));
-this.excel2.forEach(x=>console.log("excel"+x));
+    //excel associate details
+    this.excel2.push(
+      this.result.map(x => x.response[22910].AssociateDetail.associate_code)
+    );
+    this.excel2.push(
+      this.result.map(x => x.response[22910].AssociateDetail.associate_name)
+    );
+    this.excel2.push(
+      this.result.map(x => x.response[22910].AssociateDetail.email)
+    );
+    this.excel2.push(
+      this.result.map(x => x.response[22910].AssociateDetail.title)
+    );
+    this.excel2.push(
+      this.result.map(x => x.response[22910].AssociateDetail.department)
+    );
+    this.excel2.forEach(x => console.log("excel" + x));
 
- 
+    //excel calender details
     for (this.i; this.i < 30; this.i++) {
-      dateee = dateee + 1;
-      console.log("vhj" + dateee);
-      if (dateee < 10) {
-        mon = "0" + (this.fdate.getMonth() + 1);
-        datee = "0" + (this.fdate.getDate() + this.i);
-        console.log("datee" + datee + "" + this.i);
-        
-        var date = "2020-04-22";
+      getdate = getdate + 1;
+      console.log("getdate" + getdate);
+   //date < 9 to add 0
+      if (getdate < 10) {
+        finalmonth = "0" + (this.fdate.getMonth() + 1);
+        finaldate = "0" + (this.fdate.getDate() + this.i);
+        console.log("datee" + finaldate + "" + this.i);
+
+        // var date = "2020-04-22";
         this.date = JSON.parse(
-          '"' + this.fdate.getFullYear() + "-" + mon + "-" + datee + '"'
+          '"' + this.fdate.getFullYear() + "-" + finalmonth + "-" + finaldate + '"'
         );
         console.log("Date:" + this.date);
-        this.final = this.result
-          .map(x => x.response[22910].Calendar[this.date])
-          .map(x => {
+        this.final = this.result.map(x => x.response[22910].Calendar[this.date]).map(x => {
             if (x.hasOwnProperty("punch_time")) {
-               var r1=x.punch_time[0];
-                var a1 = r1.split(" ");
-           var time1 = a1[1];
+              var r1 = x.punch_time[0];
+              var a1 = r1.split(" ");
+              var time1 = a1[1];
               this.punchIn.push(time1);
-              var r2=x.punch_time[1];
+              var r2 = x.punch_time[1];
               var a = r2.split(" ");
-           var time = a[1];
+              var time = a[1];
               this.punchOut.push(time);
-
             } else {
-              console.log("abs");
+              console.log("no punch time");
             }
             this.day.push(this.date);
             this.holiday_desc.push(x.holiday_desc);
             return x;
           });
-      } else {
-        mon = "0" + (this.fdate.getMonth() + 1);
-        datee = this.fdate.getDate() + this.i;
+      }
+      //for date >9
+      else {
+        var month2 = "0" + (this.fdate.getMonth() + 1);
+        var date2 = this.fdate.getDate() + this.i;
 
         this.date = JSON.parse(
-          '"' + this.fdate.getFullYear() + "-" + mon + "-" + datee + '"'
+          '"' + this.fdate.getFullYear() + "-" + month2 + "-" + date2 + '"'
         );
         console.log("Date:" + this.date);
-        this.final = this.result
-          .map(x => x.response[22910].Calendar[this.date])
-          .map(x => {
+        this.final = this.result.map(x => x.response[22910].Calendar[this.date]).map(x => {
             if (x.hasOwnProperty("punch_time")) {
-               var r1=x.punch_time[0];
-                var a1 = r1.split(" ");
-           var time1 = a1[1];
+              var r1 = x.punch_time[0];
+              var a1 = r1.split(" ");
+              var time1 = a1[1];
               this.punchIn.push(time1);
-              var r2=x.punch_time[1];
+              var r2 = x.punch_time[1];
               var a = r2.split(" ");
-           var time = a[1];
+              var time = a[1];
               this.punchOut.push(time);
-              
             } else {
               var zero = "null";
               this.punchIn.push(zero);
               this.punchOut.push(zero);
             }
-          this.day.push(this.date);
+            this.day.push(this.date);
             this.holiday_desc.push(x.holiday_desc);
 
             return x;
           });
       }
     }
-    var j=1;
-  for (j; j <this.punchOut.length; j++) {
-      console.log("bdc");
-    this.excel[j] = [j,this.day[j],this.punchIn[j], this.punchOut[j],this.holiday_desc[j]];
+    var j = 1;
+    for (j; j < this.punchOut.length; j++) {
+      //push calender values in excel
+      this.excel[j] = [
+        j,
+        this.day[j],
+        this.punchIn[j],
+        this.punchOut[j],
+        this.holiday_desc[j]
+      ];
       console.log(this.excel[j]);
     }
-  this.excelService.generateExcel(this.excel2,this.excel);
+    //excel2 is associate details
+    //excel is calender details
+    this.excelService.generateExcel(this.excel2, this.excel);
   }
-  }
-  
-     
-
+}
